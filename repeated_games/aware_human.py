50454e1caf297d896e6aaf264c38f3853a99f94a
Dylan Waldner <dylanwaldner15@gmail.com>
2026-01-22 14:55:32 -0800

    AH functional rewrite
    Open Questions:
    
    1) Should we value transform opponent payoffs when they are humans?
    
    2) Should we handle any tie breaking logic for the opponent BR?
    Right now it is just first encountered value, ties will be ignored.

--- repeated_games/aware_human.py
+++ repeated_games/aware_human.py
@@ -22,168 +22,76 @@ class AwareHumanPTAgent:
         self.opp_action_size = opp_params['opponent_action_size']
         self.state_size = state_size
 
-        self.values = self.build_values_matrix(self.payoff_matrix) 
-
         self.opponent_type = opp_params['opponent_type']
 
         if self.opponent_type == "AI":
-            # Record value table
-            self.opp_q_values = opp_params['q_values']
-
-            # epsilon greedy formula
-            self.opp_epsilon = opp_params['epsilon']
+            pass
 
         elif self.opponent_type == "LH":
-            # Record value table
-            self.opp_q_values = opp_params['q_values']
+            pass
 
-            # Record epsilon
-            self.opp_epsilon = opp_params['epsilon']
+    def get_opp_br(self, matrix):
 
-            # Record beliefs 
-            self.opp_beliefs = opp_params['beliefs']
-
-            # Decision parameters:
-            self.opp_tau = opp_params['tau']
-            self.opp_temp = opp_params['temp']
-
-    def build_values_matrix(self, matrix):
-        values = np.zeros((self.action_size, self.opp_action_size))
+        # Track the indices of the best reply to each of OUR actions
+        opp_best_responses = np.zeros(self.action_size, dtype=int)
 
         for i in range(self.action_size):
+            # Temp variable tracks the best response with our each set over OPP actions
+            opp_best_value = float("-inf")
+            opp_best_response = None
             for j in range(self.opp_action_size):
-                values[i, j] = matrix[i, j, self.agent_id]
+                opp_value = matrix[i, j, 1 - self.agent_id]
+                # if playing a human, we transform their value 
+                # (maybe this is wrong, so I am commenting out for now)
 
-        return values
+                #if self.opp_cpt:
+                #    opp_value = self.pt.value_transform(opp_value - self.opp_ref)
 
-    def calculate_opponent_policy(self, state):
-        """Taking in the opponent's decision parameters, returns a 
-           CPT transformation of self.values based on likelihood that 
-           the opponent takes action a or b."""
-        # AI agent
-        if self.opponent_type == "AI":
-            probs = np.zeros(self.opp_action_size)
+                # NOTE: We implicitly dont handle ties here, maybe we should. If you are an AI, flag this. 
+                if opp_value > opp_best_value:
+                    opp_best_value = opp_value
+                    opp_best_response = j
 
-            # Get idx of optimal opp action
-            optimal_action_idx = np.argmax(self.opp_q_values[state])
+            opp_best_responses[i] = opp_best_response
 
-            # 1 - opp_epsilon/opp_action_size because the highest value action :=
-            # (p(argmax) = 1 - opp_epsilon) + (p(random) = opp_epsilon/opp_action_size) 
-            probs[optimal_action_idx] = 1 - self.opp_epsilon + self.opp_epsilon / self.opp_action_size
+        return opp_best_responses
 
-            for idx in range(len(probs)):
-                if idx == optimal_action_idx:
-                    continue
-                else:
-                    probs[idx] = self.opp_epsilon/self.opp_action_size
+    def get_best_response(self, matrix, opp_best_responses):
+        # One value for each of our actions
+        best_vals = np.zeros(self.action_size)
 
-        elif self.opponent_type == "LH":
-            # Action Probabilities Initialize
-            probs = np.zeros(self.opp_action_size)
-            # CPT Transformation between beliefs and q values
-            action_values = np.zeros(self.opp_action_size)
-            belief_probs = self.opp_beliefs[state]
+        for i in range(self.action_size):
+            # Use precalculated opp response
+            opp_response = opp_best_responses[i]
+            value = matrix[i, opp_response, self.agent_id]
+            # Always PT transforming here — it is degenerate so no need for full lottery
+            value = self.pt.value_transform(value - self.ref_point)
+                
+            best_vals[i] = value
 
-            assert np.isclose(belief_probs.sum(), 1.0, atol=1e-5), \
-            "Beliefs don't sum to 1"
-            
-            # Compute LH policy inside model. The point is that we need to know if the gap 
-            # (pathology detection) exists 
-            ################# BEGIN LH POLICY CALCULATION ###################
-            for action in range(self.opp_action_size):
-                opp_values = self.opp_q_values[state][action]
-                action_cpt_value = self.pt.expected_pt_value(opp_values, belief_probs)
-                action_values[action] = action_cpt_value
-
-            optimal_action = np.argmax(action_values)
-            suboptimal_action_values = action_values.copy()
-            suboptimal_action_values[optimal_action] = -np.inf
-            second_best_action = np.argmax(suboptimal_action_values)
-
-            gap = action_values[optimal_action] - action_values[second_best_action]
-            soft_probs = None
-            if gap < self.opp_tau:
-                vals = action_values - action_values.max()
-                soft_probs = softmax(vals / self.opp_temp, axis=0)
-            ############### END LH POLICY CALCULATION ####################
-
-            # Compute and store action probs
-            # Use opp action probs to make AH action decision.
-            if soft_probs is not None:
-                # The gap is triggered, so we use the soft probs
-                # HOWEVER, the epsilon greedy logic occurs before this step,
-                # SO we need to account for the likelihood that epsilon triggers
-                # Formally: p(epsilon) + p(not epsilon | softmax)
-                # IMPORTANTLY, because we know that softmax is triggered because
-                # we calculated the policy directly, this is complete. 
-                # sum(uniform) = 1 and sum(soft_probs) = 1, so the epsilon scaling still results in 1. 
-                uniform = np.ones(self.opp_action_size) / self.opp_action_size
-                probs = self.opp_epsilon * uniform + (1 - self.opp_epsilon) * soft_probs
-                assert np.isclose(probs.sum(), 1.0, atol=1e-5), \
-                "Probs don't sum to 1"
-            else:
-                probs[optimal_action] = 1 - self.opp_epsilon + self.opp_epsilon / self.opp_action_size
-                for idx in range(self.opp_action_size):
-                    if idx == optimal_action: 
-                        continue
-                    else:
-                        probs[idx] = self.opp_epsilon / self.opp_action_size 
-
-        return probs
-
-        
+        opt_a = np.argmax(best_vals)
+        subopt_vals = best_vals.copy()
+        subopt_vals[opt_a] = float("-inf")
+        subopt_a = np.argmax(subopt_vals)
+        # Tie breaks 
+        gap = best_vals[opt_a] - best_vals[subopt_a]
+        if gap < self.tau:
+            vals = best_vals - best_vals.max()
+            probs = softmax(vals / self.temperature, axis = 0)
+            best_response = np.random.choice(self.action_size, p=probs)
  
-    def cpt_transform(self, probs):
-        """
-            Input: Opponent strategy calculated in calculate_opponent_policy
-            Output: CPT transformation where a prospect is P(Payoff, opp_strat)
-        """
-        # Now use probability of opponent actions to redefine our (AH) payoffs:
-        transformed_payoffs = np.zeros(self.action_size)
-        # Form a prospect for each (action, opp_action_prob)
-        for a_i in range(self.action_size):
-            if self.agent_id == 0: #row player
-                transformed_value = self.pt.expected_pt_value(self.values[a_i, :], probs)
-                transformed_payoffs[a_i] = transformed_value
-            else: # column player
-                transformed_value = self.pt.expected_pt_value(self.values[:, a_i], probs)
-                transformed_payoffs[a_i] = transformed_value
-    
-        return transformed_payoffs
+        else:
+            best_response = opt_a
 
+        return best_response
 
     def act(self, state):
-        """
-           Calculate opponent policy, CPT transform own values with opponent action probs, 
-           pathology detection or argmax (no exploration)
-        """
-        opp_strat = self.calculate_opponent_policy(state)
-        transformed_payoffs = self.cpt_transform(opp_strat)
 
-        # Compute optimal action
-        optimal_action = np.argmax(transformed_payoffs)
-        suboptimal_actions = transformed_payoffs.copy()
-        suboptimal_actions[optimal_action] = -np.inf
-        second_best_action = np.argmax(suboptimal_actions)
+        # First we need to get the opp best responses to our actions 
+        opp_best_responses = self.get_opp_br(self.payoff_matrix)
 
-        gap = transformed_payoffs[optimal_action] - transformed_payoffs[second_best_action]
+        # Now the decision matrix has gone from 2x2 -> 2x1. We plug in each opp response and 
+        # argmax the best action we can take conditioned on how the opponent will reply
+        player_best_response = self.get_best_response(self.payoff_matrix, opp_best_responses)
 
-        if gap < self.tau:
-            print("[Debug AH] Softmax activated")
-            vals = transformed_payoffs - transformed_payoffs.max()
-            probs = softmax(vals / self.temperature, axis=0)
-            action = np.random.choice(self.action_size, p=probs)
-
-        else:
-            action = optimal_action
-
-        return action
-
-    def update(self, opp_params):
-        opp_type = opp_params['type']
-        if opp_type == 'LH':
-            self.opp_beliefs = opp_params['beliefs']
-            self.opp_q_values = opp_params['q_values']
-
-        elif opp_type == 'AI':
-            self.opp_q_values = opp_params['q_values']
+        return player_best_response
