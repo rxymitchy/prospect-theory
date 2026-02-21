c899cf29677e519ddc11b8ae0b8ca7beec6cb1cc
Dylan Waldner <dylanwaldner15@gmail.com>
2026-02-20 19:08:25 -0500

    double auction comments

--- repeated_games/double_auction.py
+++ repeated_games/double_auction.py
@@ -1,6 +1,13 @@
 import numpy as np
 # Double Auction Game
 class DoubleAuction:
+  '''
+  A double auction game with midpoint pricing. Please let me know if you think a different pricing mechanism is better.
+  other than that, very similar to the game env, using state indexing with base n^2 where n is the action size. 
+
+  Another important consideration: should we have no trade be 0 reward or -1?
+  -1 might inspire greater exploration early on
+  '''
   def __init__(self, k=10, valuation=6, cost=4, horizon=100, state_history=2):
     self.A = set(range(1, k+1))
     self.B = set(range(1, k+1))
@@ -25,10 +32,10 @@ class DoubleAuction:
         # bids and asks are 1 indexed
         bid = i + 1
         ask = j + 1
-        if bid >= ask:
-          price = (bid + ask) / 2
-          buyer_payoff = self.valuation - price
-          seller_payoff = price - self.cost
+        if bid >= ask: # trade confirmed
+          price = (bid + ask) / 2 # midpoint pricing
+          buyer_payoff = self.valuation - price # reward is distance of valiuation over price
+          seller_payoff = price - self.cost # reward is distance from cost under price (profit)
           payoff_matrix[i, j] = (buyer_payoff, seller_payoff)
         else:
           payoff_matrix[i, j] = (0, 0)
@@ -41,6 +48,10 @@ class DoubleAuction:
     return self._get_state()
 
   def _get_state(self):
+    '''
+    Part of the problem with the double auction game is the way states explode, causing memory problems. 
+    Maybe there's a better way to handle this? Games are also converging on just a few states, predictably. 
+    '''
 
     state = 0
     for i in range(len(self.history)):
@@ -68,10 +79,10 @@ class DoubleAuction:
 
     # If a successful trade
     if bid >= ask:
-      price = (bid + ask) / 2
-      buyer_payoff = self.valuation - price
-      seller_payoff = price - self.cost
+      price = (bid + ask) / 2 # midpoint pricing
+      buyer_payoff = self.valuation - price # how good a buy
+      seller_payoff = price - self.cost # profit
       return self._get_state(), buyer_payoff, seller_payoff, done, {}
 
     else:
-      return self._get_state(), 0, 0, done, {}
+      return self._get_state(), 0, 0, done, {} # Should no trade rewards be -1?
